8502a40f4cd1ee80a203492057c0343d85b5bd08
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-22 22:46:53 +0800

    - update DropDown.svelte

--- DropDown.svelte
+++ DropDown.svelte
@@ -1,10 +1,10 @@
 <script>
-		let { open = false, children } = $props();
+	let { open = false, children } = $props();
 </script>
 
 <div style={"position: relative;"}>
   <button onclick={() => {  open = !open }}>Dropdown</button>
   <div style={`display: ${open ? "block": "none"}; position: absolute; z-index: 1;`}>
-		{@render children()}
+	{@render children()}
   </div>
 </div>
