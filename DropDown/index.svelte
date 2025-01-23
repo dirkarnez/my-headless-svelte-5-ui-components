0ff1c7e375ae403ac6885370772c8c645f2a9a46
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-23 23:27:55 +0800

    - update and rename index.svelte to DropDown/index.svelte

---
+++ DropDown/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import DropDown from "./DropDown.svelte"
+</script>
+
+<DropDown>
+	<div style={"background: white"}>Link 1</div>
+	<div style={"background: white"}>Link 2</div>
+	<div style={"background: white"}>Link 3</div>
+</DropDown>
+
+<DropDown>
+	<div style={"background: white"}>Link 1</div>
+	<div style={"background: white"}>Link 2</div>
+	<div style={"background: white"}>Link 3</div>
+</DropDown>
--- index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import DropDown from "./DropDown.svelte"
-</script>
-
-<DropDown>
-		<div>Link 1</div>
-	  <div>Link 2</div>
-	  <div>Link 3</div>
-</DropDown>
