bf71609cfb8f447ec8c19d87bae148a804efe77a
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-23 23:22:03 +0800

    - update and rename Accordian.svelte to Accordian/index.svelte

---
+++ Accordian/index.svelte
@@ -0,0 +1,50 @@
+<script>
+
+	import Accordion from "./Accordion.svelte";
+	import AccordionSection from "./AccordionSection.svelte";
+  let activeSectionA;
+	let activeSectionB;
+</script>
+<Accordion>
+	<AccordionSection bind:activeSection={activeSectionA} index={0}>
+		{#snippet name()}
+			P. Sherman 1
+		{/snippet}
+		{#snippet address()}
+			42 Wallaby Way<br />
+			Sydney
+		{/snippet}
+	</AccordionSection>
+
+		<AccordionSection bind:activeSection={activeSectionA} index={1}>
+		{#snippet name()}
+			P. Sherman 2
+		{/snippet}
+		{#snippet address()}
+			42 Wallaby Way<br />
+			Sydney
+		{/snippet}
+	</AccordionSection>
+</Accordion>
+<br>
+<Accordion>
+	<AccordionSection bind:activeSection={activeSectionB} index={0}>
+		{#snippet name()}
+			P. Sherman 1
+		{/snippet}
+		{#snippet address()}
+			42 Wallaby Way<br />
+			Sydney
+		{/snippet}
+	</AccordionSection>
+
+		<AccordionSection bind:activeSection={activeSectionB} index={1}>
+		{#snippet name()}
+			P. Sherman 2
+		{/snippet}
+		{#snippet address()}
+			42 Wallaby Way<br />
+			Sydney
+		{/snippet}
+	</AccordionSection>
+</Accordion>
