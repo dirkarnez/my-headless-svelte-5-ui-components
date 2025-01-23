236d778bee444a12fb483a604d89e60b006af9ca
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-23 23:22:41 +0800

    - add Accordion/AccordionSection.svelte

---
+++ Accordian/AccordionSection.svelte
@@ -0,0 +1,12 @@
+<script>
+	let { activeSection = $bindable(), name, address, email, index  } = $props();
+</script>
+<button type="button" style={" width: 100%;"} onclick={() => {activeSection = index;}}>
+	Section {index}
+</button>
+<div style={`width: 100%; display: ${activeSection === index ? 'block' : 'none'}`}>
+	{@render name()}
+</div>
+
+    
+  
