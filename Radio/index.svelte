1ae03b61399c4730867760d875d927d86e0cb7ce
Dirk Arnez <smalldirkalex@gmail.com>
2025-08-24 18:59:24 +0800

    - add Radio/index.svelte

---
+++ Radio/index.svelte
@@ -0,0 +1,8 @@
+<!--
+<div role="radio" tabindex="-1" aria-checked="false" class="van-radio wrap-inner">
+  <div class="van-radio__icon van-radio__icon--round">
+  <i class="van-icon van-icon-success" style=""></i>
+  </div>
+	<span class="van-radio__label" style="color: rgb(51, 51, 51);">10001-20000</span>
+</div>
+-->
